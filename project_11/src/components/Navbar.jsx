f006641f079d9f17711ffcdf171aff9a9fd761ec
manuja <rpmanuja123@gmail.com>
2025-05-25 09:38:43 +0530

    hh

--- project_11/src/components/Navbar.jsx
+++ project_11/src/components/Navbar.jsx
@@ -1,15 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/navbar.css';
 
 const Navbar = () => {
+  const [activeDropdown, setActiveDropdown] = useState(null);
+
+  const menuItems = [
+    { name: 'HOME', path: '/' },
+    {
+      name: 'INTERNATIONAL',
+      path: '/international',
+      submenu: [
+        'Men\'s Cricket',
+        'Women\'s Cricket',
+        'U19 Cricket',
+        'Rankings'
+      ]
+    },
+    {
+      name: 'DOMESTIC',
+      path: '/domestic',
+      submenu: [
+        'Major Club',
+        'Premier Trophy',
+        'Premier Limited Over',
+        'First Class'
+      ]
+    },
+    { name: 'NEWS', path: '/news' },
+    {
+      name: 'MEDIA',
+      path: '/media',
+      submenu: [
+        'Photos',
+        'Videos',
+        'Press Releases'
+      ]
+    },
+    {
+      name: 'COACHING',
+      path: '/coaching',
+      submenu: [
+        'Level 1',
+        'Level 2',
+        'Level 3',
+        'High Performance'
+      ]
+    },
+    {
+      name: 'GALLERY',
+      path: '/gallery',
+      submenu: [
+        'Match Gallery',
+        'Event Gallery',
+        'Historic Moments'
+      ]
+    },
+    {
+      name: 'ABOUT US',
+      path: '/about',
+      submenu: [
+        'Overview',
+        'Executive Committee',
+        'Contact Us'
+      ]
+    }
+  ];
+
   return (
     <header className="header">
       {/* Top blue layer with utilities */}
       <div className="top-layer">
         <div className="container">
           <div className="utility-items">
-            <div>
+            <div className="search-box">
               <input
                 type="text"
                 placeholder="Search"
@@ -44,60 +108,33 @@ const Navbar = () => {
         <div className="container">
           <div className="nav-container">
             <ul className="nav-menu">
-              <li>
-                <Link to="/" className="nav-link">HOME</Link>
-              </li>
-              <li>
-                <Link to="/international" className="nav-link">
-                  INTERNATIONAL
-                  <svg className="dropdown-icon" viewBox="0 0 20 20" fill="currentColor">
-                    <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
-                  </svg>
-                </Link>
-              </li>
-              <li>
-                <Link to="/domestic" className="nav-link">
-                  DOMESTIC
-                  <svg className="dropdown-icon" viewBox="0 0 20 20" fill="currentColor">
-                    <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
-                  </svg>
-                </Link>
-              </li>
-              <li>
-                <Link to="/news" className="nav-link">NEWS</Link>
-              </li>
-              <li>
-                <Link to="/media" className="nav-link">
-                  MEDIA
-                  <svg className="dropdown-icon" viewBox="0 0 20 20" fill="currentColor">
-                    <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
-                  </svg>
-                </Link>
-              </li>
-              <li>
-                <Link to="/coaching" className="nav-link">
-                  COACHING
-                  <svg className="dropdown-icon" viewBox="0 0 20 20" fill="currentColor">
-                    <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
-                  </svg>
-                </Link>
-              </li>
-              <li>
-                <Link to="/gallery" className="nav-link">
-                  GALLERY
-                  <svg className="dropdown-icon" viewBox="0 0 20 20" fill="currentColor">
-                    <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
-                  </svg>
-                </Link>
-              </li>
-              <li>
-                <Link to="/about" className="nav-link">
-                  ABOUT US
-                  <svg className="dropdown-icon" viewBox="0 0 20 20" fill="currentColor">
-                    <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
-                  </svg>
-                </Link>
-              </li>
+              {menuItems.map((item, index) => (
+                <li key={index} 
+                    className="nav-item"
+                    onMouseEnter={() => item.submenu && setActiveDropdown(index)}
+                    onMouseLeave={() => setActiveDropdown(null)}
+                >
+                  <Link to={item.path} className="nav-link">
+                    {item.name}
+                    {item.submenu && (
+                      <svg className="dropdown-icon" viewBox="0 0 20 20" fill="currentColor">
+                        <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
+                      </svg>
+                    )}
+                  </Link>
+                  {item.submenu && activeDropdown === index && (
+                    <ul className="submenu">
+                      {item.submenu.map((subItem, subIndex) => (
+                        <li key={subIndex}>
+                          <Link to={`${item.path}/${subItem.toLowerCase().replace(/\s+/g, '-')}`} className="submenu-link">
+                            {subItem}
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  )}
+                </li>
+              ))}
             </ul>
           </div>
         </div>
